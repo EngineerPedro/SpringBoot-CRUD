505b62f4ca80c1dc61eebdcf274fc63326f1f7c8
Pedro <plguzman1993@gmail.com>
2024-06-04 08:22:33 -0600

    Made a change to the DTO. Purpose was to make sure we had to accurately delete items from the database. What I did was expose the records id to the user. They cannot change it ONLY delete / alter the desire bookName and author.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
+
 # CRUD SpringBoot (JPA) - Books
  Execute all CRUD operations with SpringBoot
--- src/main/java/com/example/Books/dto/BookDTO.java
+++ src/main/java/com/example/Books/dto/BookDTO.java
@@ -11,6 +11,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class BookDTO {
 
+    private Long id;
+
     private String bookName;
 
     private String author;
